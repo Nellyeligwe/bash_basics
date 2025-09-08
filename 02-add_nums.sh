5aec607669cf07e0364eb936b9bf532dc3a62ada
nellyeligwe <neligwe@ggc.edu>
2025-09-07 22:10:13 -0400

    HW: alternative width*height solution in 02-add_nums.sh (version B on alt_version)

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,10 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter the image width:"
+read width
+echo "Enter the image height:"
+read height
+
+pixels=$((width * height))
+echo "Total pixels calculated: $pixels"
