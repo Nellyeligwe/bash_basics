3194f53bae6b889a4b339896aec3998342bd2059
nellyeligwe <neligwe@ggc.edu>
2025-09-07 21:57:59 -0400

    HW: add width*height calculation in 02-add_nums.sh (version A on main)

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,10 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter width:"
+read width
+echo "Enter height:"
+read height
+
+pixels=$((width * height))
+echo "The total number of pixels is $pixels"
